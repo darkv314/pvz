1a9a683cb45cfa8b4e739ad44f7b52672414fb2e
Vanessa Tejerina <vanessa.tejerina@jala.university>
2022-10-20 14:19:11 -0400

    Implementar map

---
+++ scenes/plant/Plant.gd
@@ -0,0 +1,49 @@
+extends StaticBody2D
+
+class_name Plant
+
+var hp: int
+var entered = false
+var last_mouse_pos = null
+var first_click = false
+var is_clicked = false
+
+func _input(event):
+	if entered:
+		if event.is_action_pressed("plant_move"):
+			first_click = !first_click
+#			print('lolaso')
+	else:
+		if first_click && event.is_action_pressed("plant_move"):
+			first_click = false
+			position = get_viewport().get_mouse_position()
+#			print('lll')
+
+func _init(new_hp := 3):
+	hp = new_hp
+	
+func die():
+	$Sprite.visible=false
+
+func receive_dmg():
+	pass
+	
+
+func _process(delta):
+	if first_click:
+		position = get_viewport().get_mouse_position()
+#	if entered and Input.is_mouse_button_pressed(BUTTON_LEFT):
+#		position = get_viewport().get_mouse_position()
+
+
+func _on_Plant_mouse_entered():
+	entered = true
+
+func _on_Plant_mouse_exited():
+	entered = false;
+
+
+func _on_Hurtbox_area_entered(area):
+	hp-=1
+	if hp <=0:
+		die()
---
+++ scenes/plant/attackPlant/melonpult/MelonPult.gd
@@ -0,0 +1,25 @@
+extends AttackPlant
+
+const melon_path = preload("res://scenes/plant/attackPlant/projectiles/Melon.tscn")
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func shoot():
+	print('a')
+	var melon = melon_path.instance()
+	melon.position = $MelonSpawn.position
+	add_child(melon)
+	melon.launch(Vector2(350, -200))
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func _on_ShootTimer_timeout():
+	shoot()
